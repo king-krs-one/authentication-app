3fe7a31ce3f6169cf7c111d5f2d12f2d01fb559e
king-krs-one <jonathan.nickels@googlemail.com>
2023-08-11 15:03:21 +0200

    refine esbuild config, replace watch onRebuild as deprecated

--- frontend/esbuild.config.mjs
+++ frontend/esbuild.config.mjs
@@ -1,17 +1,40 @@
 import esbuild from "esbuild"
 
-esbuild.build({
-    entryPoints: ['src/Sample.ts'],
-    minify: true,
-    target: ['chrome58', 'firefox57', 'safari11', 'edge16'],
-    outfile: 'dist/bundle.js',
-    sourcemap: true,
-    watch: {
-        onRebuild: (err) => {
+const args = process.argv.slice(2)
+const watch = args.includes('--watch')
 
-        }
-    },
-    plugins: [
+const watchPlugin = {
+  name: 'watch-plugin',
+  setup(build) {
+    build.onStart(() => {
+      console.log(`Build starting: ${new Date(Date.now()).toLocaleString()}`)
+    })
+    build.onEnd((result) => {
+      if (result.errors.length > 0) {
+        console.log(`Build finished, with errors: ${new Date(Date.now()).toLocaleString}`)
+      } else {
+        console.log(`Build finished, succesfully: ${new Date(Date.now()).toLocaleString()}`)
+      }
+    })
+  }
+}
 
-    ]
-}).catch(() => process.exit(1))
+const context = await esbuild.context({
+  bundle: true,
+  entryPoints: ['src/index.tsx'],
+  minify: true,
+  outfile: 'dist/bundle.js',
+  sourcemap: true,
+  plugins: [
+    watchPlugin
+  ]
+})
+
+if (watch) {
+  await context.watch()
+  console.log('watching....')
+}
+else {
+  context.rebuild()
+  context.dispose()
+}
