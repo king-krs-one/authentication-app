7425dc5e0de96c9617acfdab82e81503eb043985
king-krs-one <jonathan.nickels@googlemail.com>
2023-08-11 09:22:04 +0200

    esbuild config refinement

--- frontend/esbuild.config.mjs
+++ frontend/esbuild.config.mjs
@@ -2,5 +2,16 @@ import esbuild from "esbuild"
 
 esbuild.build({
     entryPoints: ['src/Sample.ts'],
-    outfile: 'dist/bundle.js'
-})
+    minify: true,
+    target: ['chrome58', 'firefox57', 'safari11', 'edge16'],
+    outfile: 'dist/bundle.js',
+    sourcemap: true,
+    watch: {
+        onRebuild: (err) => {
+
+        }
+    },
+    plugins: [
+
+    ]
+}).catch(() => process.exit(1))
